cc3d571cd50c31d4f5ee0344b50d88dcea14b8fe
Tahir0918 <tk3000465@gmail.com>
2024-11-26 19:35:45 +0500

    Updated

---
+++ sell.js
@@ -0,0 +1,15 @@
+document .write( "tahir khan ")
+var tahir = prompt( "can I Help You ")
+document.write("<br>")
+if (tahir == 'TIGER' )
+{
+    window.location.href=" http://127.0.0.1:5500/TIGER.html"
+}
+var he  = prompt ( " How are you")
+document.write( "<br>")
+document.write( he )
+document.write("<br>")
+
+var tahir = prompt( " can i help you ")
+if ( tahir =="TIGER")
+{  window.location.hraf="N0 THANKS"}
